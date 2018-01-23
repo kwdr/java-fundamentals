876496a0662b756d67acd79024405353e6855912
kwdr <kwdr@tlen.pl>
2018-01-23 21:16:32 +0000

    calc v6

--- src/com/calculator/Main.java
+++ src/com/calculator/Main.java
@@ -3,14 +3,19 @@ package com.calculator;
 public class Main {
 
     public static void main(String[] args) {
-
+//
+//        MathEquation[] equations = new MathEquation[4];
+//        equations[0] = create(100.0d, 50.0d, 'd');
+//        equations[1] = create(25.0d, 92.0d, 'a');
+//        equations[2] = create(225.0d, 17.0d, 's');
+//        equations[3] = create(11.0d, 3.0d, 'm');
 
 
         MathEquation[] equations = new MathEquation[4];
-        equations[0] = create(100.0d,50.0d,'d' );
-        equations[1] = create(25.0d, 92.0d,'a');
-        equations[2] = create(225.0d, 17.0d, 's');
-        equations[3] = create(11.0d, 3.0d, 'm');
+        equations[0] = new MathEquation(100.0d, 50.0d, 'd');
+        equations[1] = new MathEquation(25.0d, 92.0d, 'a');
+        equations[2] = new MathEquation(225.0d, 17.0d, 's');
+        equations[3] = new MathEquation(11.0d, 3.0d, 'm');
 
         for (MathEquation equation : equations) {
             equation.execute();
@@ -19,10 +24,10 @@ public class Main {
         }
     }
 
-    public static MathEquation create(double leftVal, double rightVal, char opCode) {
+/*    public static MathEquation create(double leftVal, double rightVal, char opCode) {
 
         MathEquation equation = new MathEquation(leftVal, rightVal, opCode);
 
         return equation;
-    }
+    }*/
 }
